7b51927d003c9cd554a2b03bd6bc33a2a8a27fc8
unknown <s20313@sci.pdn.ac.lk>
2025-08-12 18:42:31 +0530

    Created Director dashboad

--- HostelManagementSystem/src/main/java/com/hostelManagementSystem/HostelManagementSystem/config/SecurityConfig.java
+++ HostelManagementSystem/src/main/java/com/hostelManagementSystem/HostelManagementSystem/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 public class SecurityConfig {
 
@@ -14,18 +13,15 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/signup", "/css/**", "/js/**",
-                                "/images/**","/student-dashboard","/Student_ComplainsAndRequests",
-                                "/Student_History_Damage","/Student_History_Payment","/Student_History_Residence",
-<<<<<<< HEAD
-                                "/bursar-dashboard","/vc_dvc-dashboard","/dean-dashboard","/forgot-password",
-                                "/verify-code", "/reset-password","/Student_Payments","/submitPayment",
-                                "/submitForm", "/student/uploadSlip",
-                                "/upload-excel", "/SRB_history","/Director-AccommodationDivision-dashboard").permitAll()
-=======
-                                "/bursar-dashboard","/vc_dvc-dashboard","/Deans-dashboard","/forgot-password",
-                                "/verify-code", "/reset-password","/Student_Payments","/submitPayment","/Director-AccomodationDivision-dashboard").permitAll()
->>>>>>> 63e3adfa2dc9af1c32c00a7b9273d8ccb513c943
+                        .requestMatchers(
+                                "/", "/login", "/signup", "/css/**", "/js/**",
+                                "/images/**", "/student-dashboard", "/Student_ComplainsAndRequests",
+                                "/Student_History_Damage", "/Student_History_Payment", "/Student_History_Residence",
+                                "/bursar-dashboard", "/vc_dvc-dashboard", "/dean-dashboard", "/Deans-dashboard",
+                                "/forgot-password", "/verify-code", "/reset-password",
+                                "/Student_Payments", "/submitPayment", "/submitForm", "/student/uploadSlip",
+                                "/upload-excel", "/SRB_history","/Director-AccomodationDivision-dashboard"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(logout -> logout
@@ -36,5 +32,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
